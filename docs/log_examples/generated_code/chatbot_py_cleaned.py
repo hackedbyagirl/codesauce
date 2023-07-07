cd2e4b7a2c217fe6ba0a2b4f8ca6113eace21eeb
Brette (BNastee) <56555806+hackedbyagirl@users.noreply.github.com>
2023-07-07 18:17:43 +0000

    log examples

---
+++ docs/log_examples/generated_code/chatbot_py_cleaned.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+import time
+import questionary
+from datetime import datetime
+from codesauce.utils.colors import Color
+from codesauce.config.config import Config
+from codesauce.utils.display import Display
+from codesauce.modules.function_call import FunctionCall
+from codesauce.actions.input import get_user_question, get_project_description
+from codesauce.prompts.user_prompt_builder import build_user_question_prompt, build_project_user_prompt
+from codesauce.prompts.system_prompt_builder import build_ai_assistant_prompt, build_system_prompt
+
+PROJ_TASK = 'project_task'
+PROJ_PROMPT = 'proj_prompt'
+
+
+class ChatBot(object):
+    def __init__(self):
+        self.chat_history = []
+        self.call_functions = FunctionCall(self.chat_history)
+        timestamp = datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d_%H-%M-%S")
+        self.chat_file = f"chat_log_{timestamp}.json"
+
+    def launch(self):
+        # Display banner
+        Display.display_interactive_chat_banner()
+        proj_desc = self.request_project_description()
+        if proj_desc:
+            proj_system_prompt = build_system_prompt(PROJ_TASK, PROJ_PROMPT)
+            proj_user_prompt = build_project_user_prompt(proj_desc)
+            self.chat_history.append(proj_system_prompt)
+            self.chat_history.append(proj_user_prompt)
+            self.chat_history.append(build_ai_assistant_prompt("Thank you for providing a project description as context"))
+
+    def interact(self):
+        """
+        Begins interaction with chatbot
+        """
+        # Launch initial ai interaction
+        # self.launch()
+        # Engage in Interactive chat loop
+        # Get project Description
+        Color.print(
+            "\n{B}Instructions: {W}\n\tFeel free to provide as much detail as possible when asking questions. You are able to enter multiple lines using the 'ENTER' button."
+        )
+        while True:
+            question = get_user_question()
+            if not question:
+                Config.save_chat_log(self.chat_file, self.chat_history)
+                Config.exit(1)
+            # Add question to chat history
+            user_prompt = build_user_question_prompt(question)
+            self.chat_history.append(user_prompt)
+            # Launch ai interaction
+            self.chat_history = self.call_functions.interact(self.chat_history)
+
+    ########################################################################
+    # Helper Functions
+    ########################################################################
+
+    def request_project_description(self):
+        """Requests project description"""
+        Color.print(
+            "\n{Y}Question: \n{W}Would you like to provide a general description for your project? This can help the AI system better understand what you are reviewing. If you want to provide a description, enter 'yes'. Otherwise, enter 'no'.\n"
+        )
+        response = questionary.select(
+            "Select Mode:",
+            choices=["Yes", "No"],
+        ).ask()
+        if response == "Yes":
+            proj_description = get_project_description()
+            return proj_description
+        else:
+            return False
---
+++ docs/log_examples/generated_code/generate_code_cleaned.py
@@ -0,0 +1,175 @@
+#!/usr/bin/python3
+import os
+import questionary
+from codesauce.config.config import Config
+from codesauce.utils.colors import Color
+from codesauce.modules.general_interaction import GeneralInteraction
+from codesauce.modules.function_interaction import FunctionInteraction
+from codesauce.tools.chunk_code import chunk_code, create_chunked_prompts, create_chunked_reference_prompts
+from codesauce.prompts.user_prompt_builder import (
+    build_code_generator_prompt,
+    build_multi_code_generator_prompt,
+    build_multi_code_generator_final_prompt,
+    build_code_reference_user_prompt,
+    build_multi_code_reference_final_prompt,
+    build_new_code_generator_prompt
+)
+from codesauce.prompts.system_prompt_builder import (
+    build_ai_assistant_prompt,
+    build_system_prompt
+)
+
+AI_FIRST_MESSAGE = "First Message received, continue."
+AI_MESSAGE = "Message received, continue."
+AI_FIRST_CODE_REF = "First Code Reference received, continue."
+AI_CODE_REF_MESSAGE = "Code Reference received, continue."
+CODE_GEN_TASK = 'cg_task'
+UPDATE_CODE_GEN_PROMPT = 'ucg_prompt'
+UPDATE_CODE_MULTI_PROMPT = 'ucgm_prompt'
+UPDATE_CODE_WR_PROMPT = 'ucgwr_prompt'
+NEW_CODE_GEN_PROMPT = 'ncg_prompt'
+NEW_CODE_WR_PROMPT = 'ncgwr_prompt'
+
+
+class GenerateCode(FunctionInteraction):
+    def interact(self, arguments):
+        self.arguments = arguments
+        # Check to see if instructions key exists
+        if "instructions" in self.arguments:
+            return_value = self.launch_new_code_function()
+        else:
+            return_value = self.launch_update_code_function()
+        return return_value
+
+    ########################################################################
+    # Function Specific Logic
+    ########################################################################
+
+    def launch_new_code_function(self):
+        """ Generates new code file """
+        instructions = self.arguments["instructions"]
+        new_file_name = self.arguments.get("filename")
+        references = self.arguments.get("references")
+        if not self.process_references(references, NEW_CODE_WR_PROMPT):
+            system_prompt = build_system_prompt(CODE_GEN_TASK, NEW_CODE_GEN_PROMPT)
+            self.chat_history.append(system_prompt)
+        user_prompt = build_new_code_generator_prompt(instructions)
+        self.chat_history.append(user_prompt)
+        ai_response = self.ask_ai()
+        self.save_new_code(ai_response, new_file_name)
+        function_response = {
+            "actions": "Received code generation instructions and saved response to file.",
+            "completed": True,
+        }
+        return function_response
+
+    def launch_update_code_function(self):
+        """ Updates existing code file """
+        Color.print("{B}Generating Code for Existing file ...\n")
+        update_file = self.arguments["filename"]
+        instructions = coding_task = self.arguments["coding_task"]
+        references = self.arguments.get("references")
+        prompt_functions = {
+            'initial': build_code_generator_prompt,
+            'final': build_multi_code_generator_final_prompt,
+            'intermediate': build_multi_code_generator_prompt,
+        }
+        ai_messages = {
+            'initial': AI_FIRST_MESSAGE,
+            'intermediate': AI_MESSAGE,
+        }
+        # Call the refactored function here and check the return value
+        if not self.process_references(references, UPDATE_CODE_WR_PROMPT):
+            system_prompt = build_system_prompt(CODE_GEN_TASK, UPDATE_CODE_GEN_PROMPT)
+            self.chat_history.append(system_prompt)
+        update_file_code_blocks = chunk_code(update_file)
+        if len(update_file_code_blocks) > 1:
+            updated_chat_history = create_chunked_prompts(self.chat_history, update_file_code_blocks, instructions, prompt_functions, ai_messages)
+            self.chat_history.extend(updated_chat_history)
+        else:
+            user_prompt = build_code_generator_prompt(update_file, coding_task)
+            self.chat_history.append(user_prompt)
+        function_response = {
+            "actions": "Loaded file, received generated code, and saved response to file.",
+            "completed": True,
+        }
+        return function_response
+
+    ########################################################################
+    # Code Generation Functions
+    ########################################################################
+
+    def load_file(self, file_name: str) -> str:
+        """
+        Load the code from the specified file in the current directory.
+        """
+        # Get all files in the current directory and its subdirectories
+        matches = []
+        for root, dirnames, filenames in os.walk("."):
+            for filename in filenames:
+                if filename == file_name:
+                    matches.append(os.path.join(root, filename))
+        if not matches:
+            Color.print(
+                "{Y} Warning: {W}No file with the specified name was found. Please select a different file."
+            )
+            file_path = questionary.path(
+                "Please select intended file for cleaning and optimization",
+            ).ask()
+            return str(file_path)
+        # If multiple matches were found, ask the user to select one
+        elif len(matches) > 1:
+            file_path = questionary.select(
+                "Multiple files with the specified name were found. Please select one:",
+                choices=matches,
+            ).ask()
+            return file_path
+        else:
+            return matches[0]
+
+    def ask_ai(self):
+        ai_bot = GeneralInteraction(self.chat_history)
+        ai_bot.interact(self.chat_history)
+        last_ai_response = self.chat_history[-1]
+        return last_ai_response
+
+    ########################################################################
+    # Helper Functions
+    ########################################################################
+
+    def process_references(self, references, prompt_type):
+        """ Process the references """
+        if references:
+            system_prompt = build_system_prompt(CODE_GEN_TASK, prompt_type)
+            self.chat_history.append(system_prompt)
+            for ref in references:
+                ref_file_path = self.load_file(ref)
+                self.update_chat_history_with_reference(ref_file_path)
+            return True
+        else:
+            return False
+
+    def update_chat_history_with_reference(self, file_path: str):
+        ref_prompts = {
+            'initial': build_code_reference_user_prompt,
+            'final': build_multi_code_reference_final_prompt,
+            'intermediate': build_code_reference_user_prompt,
+        }
+        ai_ref_messages = {
+            'initial': AI_FIRST_CODE_REF,
+            'final': AI_CODE_REF_MESSAGE,
+            'intermediate': AI_CODE_REF_MESSAGE,
+        }
+        ref_code_blocks = chunk_code(file_path)
+        if len(ref_code_blocks) > 1:
+            updated_chat_history = create_chunked_reference_prompts(self.chat_history, ref_code_blocks, ref_prompts, ai_ref_messages)
+            self.chat_history.extend(updated_chat_history)
+        else:
+            user_prompt = build_code_reference_user_prompt(ref_code_blocks[0])
+            ai_prompt_message = build_ai_assistant_prompt("AI_CODE_REF_MESSAGE")
+            self.chat_history.append(user_prompt)
+            self.chat_history.append(ai_prompt_message)
+
+    def save_updated_response(self, ai_response: dict, filename: str, file_path: str):
+        content = ai_response["content"]
+        updated_file = content.split("
